e6d953f46bfb7c937f3e753150e4c2c65d7973d6
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-03-02 21:20:35 -0500

    started creating a post object

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import './index.css';
+import Post from './App';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+ReactDOM.render(<Post />, document.getElementById('root'));
 registerServiceWorker();
