c28fd3dfbb25f56daec31dc4ee3c2acb219d1f35
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-03-03 00:47:48 -0500

    converted the comments to the same map mehtod calls

--- src/App.js
+++ src/App.js
@@ -7,17 +7,23 @@ import Authors from './Authors.js';
 
 class Post extends Component {
   render() {
-    let allComments = [
-      <Comments body={this.props.comments[0]} />,
-      <Comments body={this.props.comments[1]} />,
-      <Comments body={this.props.comments[2]} />
-    ];
+    // let allComments = [
+    //   <Comments body={this.props.comments[0]} />,
+    //   <Comments body={this.props.comments[1]} />,
+    //   <Comments body={this.props.comments[2]} />
+    // ];
 
     // let allAuthors = [
     //   <Authors body={this.props.authors[0]} />,
     //   <Authors body={this.props.authors[1]} />,
     //   <Authors body={this.props.authors[2]} />,
     // ];
+
+    let allComments = this.props.comments.map(
+      (comment, index) => (
+        <Comments body={comment} key={index} />
+      )
+    )
 
     let allAuthors = this.props.authors.map((author, index) => (
       < Authors body={author} key={index} />
