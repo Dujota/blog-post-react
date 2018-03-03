aa702e9c801303cb78eb331caab0d1340389ff4d
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-03-02 22:38:17 -0500

    comment component created

---
+++ src/Comments.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Comments extends Component {
+  render() {
+    return (
+      <div>
+        <p>{this.props.body}</p>
+      </div>
+    )
+  }
+}
+
+export default Comment
+
+
+
